7cb4020ce423e04f3166b6e7b9ff8824e9ee6964
root <root@psdemo141.ps.maprtech.com>
2018-08-02 00:40:34 -0700

    Automated commit Thu Aug  2 00:40:34 PDT 2018

--- start_sync.sh
+++ start_sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /root/docsync
-wget -r -A html --no-parent https://maprdocs.mapr.com/home/
-find maprdocs.mapr.com -name "*.html" | xargs -L 1 -I '{1}' dirname {1} | xargs -L 1 -I {2} mkdir -p 'git/{2}'
-find maprdocs.mapr.com -name "*.html" | xargs -L 1 -I '{1}' dirname {1} | xargs -L 1 -I {2} mkdir -p 'tmp/{2}'
-find maprdocs.mapr.com/ -name "*.html" | xargs -L 1 -I '{}' sed -n '/<main role="main">/,/<\/main>/w tmp/{}' {}
-find maprdocs.mapr.com -name "*.html" | xargs -L 1 -I '{}' sh -c "lynx -assume_charset=utf-8 -dump tmp/{} > git/{}"
+wget -r -A html --no-parent https://mapr.com/docs/home/
+find mapr.com -name "*.html" | xargs -L 1 -I '{1}' dirname {1} | xargs -L 1 -I {2} mkdir -p 'git/{2}'
+find mapr.com -name "*.html" | xargs -L 1 -I '{1}' dirname {1} | xargs -L 1 -I {2} mkdir -p 'tmp/{2}'
+find mapr.com/ -name "*.html" | xargs -L 1 -I '{}' sed -n '/<main role="main">/,/<\/main>/w tmp/{}' {}
+find mapr.com -name "*.html" | xargs -L 1 -I '{}' sh -c "lynx -assume_charset=utf-8 -dump tmp/{} > git/{}"
 cd git
 git add .
 git commit -am "Automated commit $(date)"
